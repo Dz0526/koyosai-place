9f5fdba3cb8b52448d8aaf7100e1772695ac46ed
Dz0526 <this@dz99.me>
2022-10-29 15:41:23 +0900

    WIP: add adming page

---
+++ src/pages/admin.tsx
@@ -0,0 +1,12 @@
+import { Header } from 'components/Header';
+
+const AdminPage = () => {
+  return (
+    <>
+      <Header title='編集ページ' />
+      <main></main>
+    </>
+  );
+};
+
+export default AdminPage;
